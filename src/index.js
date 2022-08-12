a37583f1bd199b9304c8d4ea8cfe2967daeafa95
galina_ka <galina_riabkova@mail.ru>
2022-08-12 15:51:24 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let m = n.toString().split('');
+    if (m[0] === '-') {
+      return Number(m.slice(1).reverse().join(''));
+    } else
+    return Number(m.reverse().join(''));
+
 }
